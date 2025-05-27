1821fe89b0f2cfea7c7d49ea14cc0e965775fdd8
DouglasNeres <douglasneres06@gmail.com>
2025-05-27 02:26:01 -0300

    init: Project start, first part part of the layout on the right in process

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,133 @@
+import { Heart, Share2, ShoppingCart } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+    <div className="min-h-screen font-[family-name:var(--font-geist-sans)]">
+      <main className="flex p-4">
+        <section className="w-[50%]">
+          <div className="flex items-center justify-center h-full">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src="/images/tenis-esportivo-premium-air-max.png"
+              alt="Tênis Esportivo Premium Air Max"
+              width={400}
+              height={400}
+              className="object-contain"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+          </div>
+        </section>
+        <section className="w-[50%]">
+          <div className="border-b border-solid border-gray-200 py-6">
+            <div className="flex items-center gap-1">
+              <span className="text-sm">4.8</span>
+              <span className="text-sm">(127 avaliações)</span>
+            </div>
+            <div className="flex flex-col gap-2">
+              <h1 className="text-2xl font-bold mt-2">
+                Tênis Esportivo Premium Air Max
+              </h1>
+              <div className="flex items-center gap-3">
+                <h1 className="text-green-600 text-2xl font-bold">R$999,99</h1>
+                <span className="text-gray-600 line-through">R$1299,99</span>
+                <span className="bg-red-500 rounded-2xl text-center text-white font-medium px-4 text-sm">
+                  -23%
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col gap-3 border-b border-solid border-gray-200 py-6">
+            <h2 className="text-lg font-semibold mt-4">Descrição</h2>
+            <p className="text-gray-700">
+              O Tênis Esportivo Premium Air Max é ideal para quem busca conforto
+              e estilo. Com tecnologia de amortecimento, ele proporciona uma
+              experiência única durante a prática de esportes ou no dia a dia.
+            </p>
+          </div>
+          <form action="">
+            <div className="flex flex-col gap-3 border-b border-solid border-gray-200 py-6">
+              <div className="flex items-center justify-between">
+                <h2 className="text-lg font-semibold mt-4">Tamanho</h2>
+                <span className="text-sm text-gray-500">Selecionado: 38</span>
+              </div>
+              <div className="flex items-center gap-3">
+                <button
+                  type="button"
+                  className="text-sm text-gray-500 font-medium bg-white shadow rounded-md px-6 py-5 hover:cursor-pointer hover:bg-gray-100">
+                  38
+                </button>
+                <button
+                  type="button"
+                  className="text-sm text-gray-500 font-medium bg-white shadow rounded-md px-6 py-5 hover:cursor-pointer hover:bg-gray-100">
+                  39
+                </button>
+                <button
+                  type="button"
+                  className="text-sm text-gray-500 font-medium bg-white shadow rounded-md px-6 py-5 hover:cursor-pointer hover:bg-gray-100">
+                  40
+                </button>
+              </div>
+              <span className="text-red-400 text-sm">* Selecione uma cor</span>
+            </div>
+            <div className="flex flex-col gap-3 border-b border-solid border-gray-200 py-6">
+              <div className="flex items-center justify-between">
+                <h2 className="text-lg font-semibold mt-4">Cor</h2>
+                <span className="text-sm text-gray-500">
+                  Selecionado: Laranja
+                </span>
+              </div>
+              <div className="flex items-center gap-3">
+                <button
+                  type="button"
+                  className="flex items-center gap-2 text-[12px] text-gray-500 font-medium bg-white shadow rounded-md px-6 py-2 hover:cursor-pointer hover:bg-gray-100">
+                  <div className="bg-black w-4 h-4 rounded-full"></div>
+                  Preto
+                </button>
+                <button
+                  type="button"
+                  className="flex items-center gap-2 text-[12px] text-gray-500 font-medium bg-white shadow rounded-md px-6 py-2 hover:cursor-pointer hover:bg-gray-100">
+                  <div className="bg-white border border-gray-300 w-4 h-4 rounded-full"></div>
+                  Branco
+                </button>
+                <button
+                  type="button"
+                  className="flex items-center gap-2 text-[12px] text-gray-500 font-medium bg-white shadow rounded-md px-6 py-2 hover:cursor-pointer hover:bg-gray-100">
+                  <div className="bg-orange-400 w-4 h-4 rounded-full"></div>
+                  Laranja
+                </button>
+                <button
+                  type="button"
+                  className="flex items-center gap-2 text-[12px] text-gray-500 font-medium bg-white shadow rounded-md px-6 py-2 hover:cursor-pointer hover:bg-gray-100">
+                  <div className="bg-purple-400 w-4 h-4 rounded-full"></div>
+                  Roxo
+                </button>
+              </div>
+              <span className="text-red-400 text-sm">* Selecione uma cor</span>
+            </div>
+            <div className="space-y-4 w-full ">
+              <div className="flex items-center gap-3 mt-4">
+                <button
+                  disabled
+                  type="submit"
+                  className="flex-1 flex items-center justify-center gap-3 disabled:bg-blue-300 bg-blue-600 text-sm text-white font-medium px-8 py-2 rounded-md border-none hover:cursor-pointer hover:bg-blue-700 transition-colors">
+                  <ShoppingCart size={18} />
+                  Adicionar ao carrinho
+                </button>
+                <button className="text-gray-700 hover:text-black hover:cursor-pointer hover:bg-gray-100 py-2 px-7 rounded-md transition-colors border border-gray-300">
+                  <Heart size={18} />
+                </button>
+                <button className="text-gray-700 hover:text-black hover:cursor-pointer hover:bg-gray-100 py-2 px-7 rounded-md transition-colors border border-gray-300">
+                  <Share2 size={18} />
+                </button>
+              </div>
+              <button
+                type="button"
+                className="w-full bg-transparent text-gray-700 text-sm font-medium px-8 py-2 rounded transition-colors border border-gray-300 hover:cursor-pointer hover:bg-gray-100">
+                Comprar agora
+              </button>
+            </div>
+          </form>
+        </section>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
